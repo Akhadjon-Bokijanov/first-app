97b746d07db96e12623bc6cdf67fbd9834e93c7a
Wiut student <cw1.fit.00007510@gmail.com>
2020-05-16 17:33:01 +0500

    Adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { SearchBox } from "./components/search-box/search-box.component"
+import {CardList} from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  constructor()
+  {
+    super();
+    this.state =
+    {
+      monsters: [],
+      searchField: ""
+    };
+  }
+
+  handleChange = (e) =>
+  {
+    this.setState({searchField: e.target.value})
+  }
+
+  componentDidMount()
+  {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  render()
+  {
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )    
+    return (
+      <div className="App">
+        <h1>Bigelow Rules</h1>
+        <SearchBox
+          placeholder="Search monsters"
+          eventHandler={ this.handleChange }
+        />
+        <CardList monsters={ filterMonsters }/>
+      </div>
+    );
+  }
 }
 
 export default App;
